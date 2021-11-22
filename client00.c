1dedaa3af48dabbfde953812c15791bc3ae57d42
jv813yh <56434164+jv813yh@users.noreply.github.com>
2021-11-22 16:19:01 +0100

    Update client00.c

--- client00.c
+++ client00.c
@@ -296,9 +296,10 @@ salt_ret_t salt_read_begin_pom(salt_channel_t *p_channel,
     	salt_err_t err_code = salt_read_init(header[0], p_buffer, size, p_msg);
     	SALT_VERIFY(err_code == SALT_ERR_NONE, err_code);
       	
-      	//Clear message is displayed on the screen
-    	//printf("\nServer:\n%.*s\n", size, p_buffer);
-   	 	//printf("\n");
+      	//Clear message from server(other clients) is displayed on the screen
+    	printf("\nMessage from client:\n%.*s\n", size, p_buffer);
+   	 	printf("\n");
+
 
     }
  
